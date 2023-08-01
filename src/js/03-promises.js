20a4b7a6e0e9a92b9e08fff6b4d9f55f2ac7ab08
DianaShylova <torfinn.karlseffny@gmail.com>
2023-08-01 16:26:51 +0300

    My 1st hw9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,40 +6,45 @@ const amountInp = document.querySelector("[name='amount']");
 const createBtn = document.querySelector("button");
 
 
-createBtn.addEventListener("submit", onclickCreateBtn);
+form.addEventListener("submit", onclickCreateBtn);
 
 function onclickCreateBtn(e) {
   e.preventDefault();
-  e.stopPropagation();
-  const delay = delayInp.value;
-  const step = stepInp.value;
-  const amount = amountInp.value;
+ 
+  let delay = Number(e.target.elements.delay.value);
+  const step = Number(e.target.elements.step.value);
+  const amount = e.target.elements.amount.value;
   
 
-  for (let i = 0; i < amount.value; i += 1) {
-    let countDelay = Number(delay) + Number(step) * i;
-    createPromise(i + 1, countDelay)
+  for (let i = 1; i <= amount; i += 1) {
+   // let countDelay = Number(delay) + Number(step) * i;
+    createPromise(i, delay)
       .then(({ position, delay }) => {
         console.log(`Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         console.log(`Rejected promise ${position} in ${delay}ms`);
       });
-  }
+    delay += step;
 }
+  }
+  
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
-  
-    if (shouldResolve) {
-      setTimeout(() => {
+    setTimeout(() => {
+     if (shouldResolve) {
+      
         resolve({ position, delay });
-      }, delay);
+      
     } else {
-      setTimeout(() => {
+      
         reject({ position, delay });
-      }, delay);
-    }
+     
+    }  
+     }, delay) 
+    
   });  
 }
+console.log(123);
